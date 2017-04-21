3885913cac1bd8775a1057d9271c9f7568101b70
frodo821 <1234567890.sakai.jp@gmail.com>
2017-04-21 20:15:05 +0900

    Update AirValveControl.ino

--- AirValveControl.ino
+++ AirValveControl.ino
@@ -1,6 +1,13 @@
 #define AIR_PIN 2
+#define AUD_IN 0
+#define IN_MIN_VALUE 256
 
 void setup()
 {
-    
+    pinMode(AIR_PIN, OUTPUT);
+}
+
+void loop(){
+    int val = analogRead(AUD_IN);
+    digitalWrite(AUD_PIN, (val > IN_MIN_VALUE));
 }
