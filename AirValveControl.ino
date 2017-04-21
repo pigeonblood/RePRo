71b2b95db896563db465d57be38a3b98e482ff20
frodo821 <1234567890.sakai.jp@gmail.com>
2017-04-21 20:18:19 +0900

    Update AirValveControl.ino

--- AirValveControl.ino
+++ AirValveControl.ino
@@ -1,4 +1,4 @@
-#define AIR_PIN 2
+ #define AIR_PIN 2
 #define AUD_IN 0
 #define IN_MIN_VALUE 256
 
@@ -9,5 +9,5 @@ void setup()
 
 void loop(){
     int val = analogRead(AUD_IN);
-    digitalWrite(AUD_PIN, (val > IN_MIN_VALUE));
+    digitalWrite(AUD_PIN, (val > IN_MIN_VALUE)); //C言語では、比較演算子は1または0をint値で返す。
 }
