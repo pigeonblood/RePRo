d88553ca5a6e24d980c3ecce76c491665ab19949
frodo821 <1234567890.sakai.jp@gmail.com>
2017-04-21 20:38:58 +0900

    Update AirValveControl.ino

--- AirValveControl.ino
+++ AirValveControl.ino
@@ -1,17 +1,41 @@
- #define AIR_PIN 2
-#define AUD_IN 0
-#define IN_MIN_VALUE 256
+#define LOG_OUT 1
+#define FFT_N 256
+#define AIR_CV 1
 
-void setup()
-{
-    pinMode(AIR_PIN, OUTPUT);
+#include <FFT.h> 
+
+void setup() {
+  Serial.begin(115200);
+  for(int i = 0; i < 9; i++){
+    pinMode(i + 1, OUTPUT);
+  }
+  TIMSK0 = 0;
+  ADCSRA = 0xe5;
+  ADMUX = 0x40;
+  DIDR0 = 0x01;
 }
 
-void loop(){
-    int val = analogRead(AUD_IN);
-    if(val > IN_MIN_VALUE || -val > IN_MIN_VALUE){
-        digitalWrite(AIR_PIN, HIGH);
-    }else{
-        digitalWrite(AIR_PIN,LOW);
-    }
+void loop() {
+  cli();
+  for (int i = 0 ; i < 512 ; i += 2) {
+    while(!(ADCSRA & 0x10));
+    ADCSRA = 0xf5;
+    byte m = ADCL;
+    byte j = ADCH;
+    int k = (j << 8) | m;
+    k -= 0x0200;
+    k <<= 6;
+    fft_input[i] = k;
+    fft_input[i+1] = 0;
+  }
+  fft_window();
+  fft_reorder();
+  fft_run();
+  fft_mag_log();
+  sei();
+  if(fft_log_out == 0){
+  	digitalWrite(AIR_CV, LOW);
+  }else{
+  	digitalWrite(AIR_CV, HIGH);
+  }
 }
